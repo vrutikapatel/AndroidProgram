eb6cc2bc85e8dd2d792d9906df4bbe85cc3a4772
vrutikapatel <77319555+vrutikapatel@users.noreply.github.com>
2021-02-06 23:30:52 +0530

    Navigation Simple Pass Data.

---
+++ Navigation_Simple_Pass_Data/app/src/main/java/com/example/navigation_simple_pass_data/MainFragment.java
@@ -0,0 +1,48 @@
+package com.example.navigation_simple_pass_data;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+
+        final NavController navController= Navigation.findNavController(view);
+
+        Button button=getView().findViewById(R.id.btn);
+        EditText editText=getView().findViewById(R.id.editText);
+
+        button.setOnClickListener((v) -> {
+
+            Navigation.findNavController(v).navigate(MainFragmentDirections.actionMainFragmentToSecondFragment().setUserName(editText.getText().toString()));
+
+
+        });
+    }
+
+}
