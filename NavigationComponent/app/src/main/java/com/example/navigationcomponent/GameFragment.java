9c28c3838551d7e5c10dfb336c65a0ca5ccb6c79
vrutikapatel <77319555+vrutikapatel@users.noreply.github.com>
2021-02-06 23:56:16 +0530

    Navigation Component.

---
+++ NavigationComponent/app/src/main/java/com/example/navigationcomponent/GameFragment.java
@@ -0,0 +1,43 @@
+package com.example.navigationcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class GameFragment extends Fragment {
+
+    public GameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.finishgameBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_gameFragment_to_endFragment);
+            }
+        });
+
+    }
+}
